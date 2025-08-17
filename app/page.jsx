e169733397b11e22de40e34ee4e90aa66827ca9e
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-08-17 13:05:02 +0530

    hero section completed

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import HeroSection from '@/components/hero'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className='grid-background'>
+       <HeroSection/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
